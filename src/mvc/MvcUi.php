4395f1abdcf7dd98d52409f761f3c3f24a447d38
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-11-17 15:34:54 +0100

    added a MvcUi class to replace the old Ui class

---
+++ src/mvc/MvcUi.php
@@ -0,0 +1,71 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+
+ /**
+  * A ui block with the configuration of one ore more ui elements
+  * Is used to configurate list elements or form masks
+  * 
+  * Is a subclass of the view
+  * 
+  * @package WebFrap
+  * @subpackage tech_core
+  */
+class MvcUi
+  extends BaseChild
+{
+////////////////////////////////////////////////////////////////////////////////
+// Attribute
+////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Normaly only one model is required,
+   * if you need more add it in your extending class
+   * 
+   * @var MvcModel
+   */
+  protected $model = null;
+
+////////////////////////////////////////////////////////////////////////////////
+// getter & setter
+////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @param MvcModel $model
+   */
+  public function setModel( $model )
+  {
+    $this->model = $model;
+  }//end public function setModel */
+
+  
+  /**
+   * @param Base $env
+   */
+  public function __construct( $env = null )
+  {
+
+    if( !$env )
+      $env = Webfrap::getActive();
+      
+    $this->env = $env;
+
+  }//end public function __construct */
+
+
+}//end class MvcUi
+
