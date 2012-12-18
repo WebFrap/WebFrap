194c7bacac2a1725feff7643567aaa331191e0ad
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-18 13:37:11 +0100

    added a mockup response controller for not yet implemented features in mockup uis

---
+++ module/webfrap/mockup/WebfrapMockup_Controller.php
@@ -0,0 +1,85 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+
+/**
+ * @package WebFrap
+ * @subpackage Core
+ * @author Dominik Bonsch
+ * @copyright Dominik Bonsch <dominik.bonsch@webfrap.net>
+ */
+class WebfrapMockup_Controller
+  extends MvcController
+{
+////////////////////////////////////////////////////////////////////////////////
+// Attributes
+////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Mit den Options wird der zugriff auf die Service Methoden konfiguriert
+   * 
+   * method: Der Service kann nur mit den im Array vorhandenen HTTP Methoden
+   *   aufgerufen werden. Wenn eine falsche Methode verwendet wird, gibt das 
+   *   System automatisch eine "Method not Allowed" Fehlermeldung zurück
+   * 
+   * views: Die Viewtypen die erlaubt sind. Wenn mit einem nicht definierten
+   *   Viewtype auf einen Service zugegriffen wird, gibt das System automatisch
+   *  eine "Invalid Request" Fehlerseite mit einer Detailierten Meldung, und der
+   *  Information welche Services Viewtypen valide sind, zurück
+   *  
+   * public: boolean wert, ob der Service auch ohne Login aufgerufen werden darf
+   *   wenn nicht vorhanden ist die Seite per default nur mit Login zu erreichen
+   * 
+   * @var array
+   */
+  protected $options           = array
+  (
+    'notyetimplemented' => array
+    (
+      'method'    => array( 'GET' ),
+      'views'      => array( 'ajax' )
+    ),
+  );
+  
+////////////////////////////////////////////////////////////////////////////////
+// Base Methodes
+////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @param LibRequestHttp $request
+   * @param LibResponseHttp $response
+   * @return void
+   */
+  public function service_notYetImplemented( $request, $response )
+  {
+    
+    
+
+    $response->addError(<<<RESP
+You clicked on a placeholder action for a not yet implemented feature.
+If this is a demo system everything is allright.<br />
+If you found this link / button in a productive system please report it to the 
+support.<br />
+    
+RESP
+);
+
+  }//end public function service_notYetImplemented */
+
+} // end class WebfrapMockup_Controller
+
+
