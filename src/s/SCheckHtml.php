3fa44b1df9f7ecfa2e42903c8a11e09f754d92d5
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-18 21:39:12 +0100

    Simple Checks for HTML Elements

---
+++ src/s/SCheckHtml.php
@@ -0,0 +1,44 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+/**
+ * @package WebFrap
+ * @subpackage tech_core
+ */
+class SCheckHtml
+{
+
+  /**
+   * @param string $allClasses
+   * @param string $classToCheck
+   */
+  public static function hasClass( $allClasses, $classToCheck )
+  {
+    
+    $tmp = explode( ' ', $allClasses );
+    
+    if( in_array( $classToCheck, $tmp ) )
+      return true;
+      
+    return false;
+    
+  }//end public static function hasClass */
+  
+
+}// end class SCheckHtml
+
+
