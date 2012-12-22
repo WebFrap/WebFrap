20b43f1297f80c1df436e7734336290c88843aa7
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-22 01:31:02 +0100

    implemented toActionExt and toUrlExt in the CRUD context

--- src/context/ContextCrud.php
+++ src/context/ContextCrud.php
@@ -104,6 +104,124 @@ class ContextCrud
     $this->categories = array();
     
   }//end public function interpretRequest */
+  
+  
+  /**
+   * @return string
+   */
+  public function toUrlExt()
+  {
+    
+    if( $this->urlExt )
+      return $this->urlExt;
+    
+    if( $this->aclRoot )
+      $this->urlExt .= '&amp;a_root='.$this->aclRoot;
+    
+    if( $this->aclRootId )
+      $this->urlExt .= '&amp;a_root_id='.$this->aclRootId;
+    
+    if( $this->aclKey )
+      $this->urlExt .= '&amp;a_key='.$this->aclKey;
+    
+    if( $this->aclNode )
+      $this->urlExt .= '&amp;a_node='.$this->aclNode;
+    
+    if( $this->aclLevel )
+      $this->urlExt .= '&amp;a_level='.$this->aclLevel;
+    
+    if( $this->refId )
+      $this->urlExt .= '&amp;refid='.$this->refId;
+    
+    if( $this->requestedBy )
+      $this->urlExt .= '&amp;rqtby='.$this->requestedBy;
+    
+    if( $this->ltype )
+      $this->urlExt .= '&amp;ltype='.$this->ltype;
+    
+    if( $this->contextKey )
+      $this->urlExt .= '&amp;cntk='.$this->contextKey;
+    
+    if( $this->maskRoot )
+      $this->urlExt .= '&amp;m_root='.$this->maskRoot;
+    
+    if( $this->publish )
+      $this->urlExt .= '&amp;publish='.$this->publish;
+    
+    if( $this->targetId )
+      $this->urlExt .= '&amp;target_id='.$this->targetId;
+    
+    if( $this->target )
+      $this->urlExt .= '&amp;target='.$this->target;
+    
+    if( $this->targetMask )
+      $this->urlExt .= '&amp;target_mask='.$this->targetMask;
+    
+    if( $this->viewId )
+      $this->urlExt .= '&amp;view_id='.$this->viewId;
+
+
+    return $this->urlExt;
+      
+  }//end public function toUrlExt */
+  
+  /**
+   * @return string
+   */
+  public function toActionExt()
+  {
+    
+    if( $this->actionExt )
+      return $this->actionExt;
+
+    if( $this->aclRoot )
+      $this->actionExt .= '&a_root='.$this->aclRoot;
+    
+    if( $this->aclRootId )
+      $this->actionExt .= '&a_root_id='.$this->aclRootId;
+    
+    if( $this->aclKey )
+      $this->actionExt .= '&a_key='.$this->aclKey;
+    
+    if( $this->aclNode )
+      $this->actionExt .= '&a_node='.$this->aclNode;
+    
+    if( $this->aclLevel )
+      $this->actionExt .= '&a_level='.$this->aclLevel;
+    
+    if( $this->refId )
+      $this->actionExt .= '&refid='.$this->refId;
+    
+    if( $this->requestedBy )
+      $this->actionExt .= '&rqtby='.$this->requestedBy;
+    
+    if( $this->ltype )
+      $this->actionExt .= '&ltype='.$this->ltype;
+    
+    if( $this->contextKey )
+      $this->actionExt .= '&cntk='.$this->contextKey;
+    
+    if( $this->maskRoot )
+      $this->actionExt .= '&m_root='.$this->maskRoot;
+    
+    if( $this->publish )
+      $this->actionExt .= '&publish='.$this->publish;
+    
+    if( $this->targetId )
+      $this->actionExt .= '&target_id='.$this->targetId;
+    
+    if( $this->target )
+      $this->actionExt .= '&target='.$this->target;
+    
+    if( $this->targetMask )
+      $this->actionExt .= '&target_mask='.$this->targetMask;
+    
+    if( $this->viewId )
+      $this->actionExt .= '&view_id='.$this->viewId;
+
+    return $this->actionExt;
+    
+  }//end public function toActionExt */
 
 } // end class ContextCrud
 
