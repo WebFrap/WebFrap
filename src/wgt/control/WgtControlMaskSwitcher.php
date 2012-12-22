2742ecba8d9b6ee3dfbc145cfe3b9401d0f58341
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-22 01:32:06 +0100

    added base methodes

--- src/wgt/control/WgtControlMaskSwitcher.php
+++ src/wgt/control/WgtControlMaskSwitcher.php
@@ -26,6 +26,40 @@ class WgtControlMaskSwitcher
 // Attributes
 ////////////////////////////////////////////////////////////////////////////////
 
+  
+  /**
+   * Rendern des Menüs
+   * @param WgtMaintab $view
+   * @param WgtDropmenu $menu
+   * @param string $context
+   * @param Entity $entity
+   * @param string $maskName
+   */
+  public function renderMenu( $view, $menu, $context, $entity, $maskName )
+  {
+
+  }//end public function renderMenu */
+ 
+  
+  /**
+   * Render des Javascripts
+   * @param WgtMaintab $view
+   * @param WgtDropmenu $menu
+   */
+  public function renderActions( $view,  $menu )
+  {
+    
+    $html = <<<HTML
+
+    self.getObject().find('.{$menu->id}-maskswitcher').change( function(){
+    	\$R.get(\$S(this).val());
+  	});
+    
+HTML;
+    
+    return $html;
+    
+  }//end public function renderActions */
 
 
 } // end class WgtControlMaskSwitcher
