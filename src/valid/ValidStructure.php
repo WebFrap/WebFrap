84680b38f090de19dd9a148b660f1858f569c044
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-24 00:07:12 +0100

    Valid Structur now supports error counter, and has a response object as attribute

--- src/valid/ValidStructure.php
+++ src/valid/ValidStructure.php
@@ -26,6 +26,66 @@ class ValidStructure
 // Attributes
 ////////////////////////////////////////////////////////////////////////////////
 
+  /**
+   * @var array
+   */
+  public $data = array();
+  
+  /**
+   * @var LibResponseHttp
+   */
+  public $response = null;
+  
+  /**
+   * Flag ob es fehler gab
+   * @var boolean
+   */
+  public $hasError = false;
+  
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+////////////////////////////////////////////////////////////////////////////////
+  
+  /**
+   * @var LibResponseHttp $response
+   */
+  public function __construct( $response )
+  {
+    $this->response = $response;
+  }//end public function __construct */
+  
+////////////////////////////////////////////////////////////////////////////////
+// Method
+////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @param string $message
+   */
+  public function addError( $message )
+  {
+
+    $this->hasError = true;
+    $this->response->addError( $message );
+    
+  }//end public function addError */
+  
+  /**
+   * @param string $key
+   * @param string $subKey
+   * @return array | null
+   */
+  public function getData( $key = null, $subKey = null )
+  {
+    
+    if( !is_null( $subKey ) )
+      return isset( $this->data[$key][$subKey] )?$this->data[$key][$subKey]:null;
+      
+    if( !is_null( $key ) )
+      return isset( $this->data[$key] )?$this->data[$key]:array();
+      
+     return $this->data;
+    
+  }//end public function getData */
 
 } // end class ValidStructure
 
