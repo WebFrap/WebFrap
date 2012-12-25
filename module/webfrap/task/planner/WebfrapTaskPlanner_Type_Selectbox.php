eb6ac70e42e5b21d2834163f987019ca9fcf0098
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-25 10:54:36 +0100

    added planner type selectbox

---
+++ module/webfrap/task/planner/WebfrapTaskPlanner_Type_Selectbox.php
@@ -0,0 +1,31 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+
+/**
+ * @package WebFrap
+ * @subpackage Taskplanner
+ * @author Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @copyright Webfrap Developer Network <contact@webfrap.net>
+ */
+class WebfrapTaskPlanner_Type_Selectbox
+  extends WgtSelectboxEnum
+{
+
+
+}//end class WebfrapTaskPlanner_Type_Selectbox
+
