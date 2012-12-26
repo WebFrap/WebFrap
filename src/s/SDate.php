346144efefd1d2d6d7529cd3c4f14558975484dc
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-27 00:29:50 +0100

    extended date

--- src/s/SDate.php
+++ src/s/SDate.php
@@ -22,6 +22,18 @@
 class SDate
 {
 
+  public static $monthDays = array
+  (
+    'jan' => 31,
+    1 => 31,
+    'feb' => 28,
+    2 => 28,
+    'mar' => 31,
+    3 => 31,
+    'mar' => 31,
+    3 => 31,
+  );
+  
   /**
    * get the actual time in standard format
    *
@@ -97,6 +109,60 @@ class SDate
     }
     
   }//end public static function getDate( $format = 'Y-m-d' )
+  
+  
+  /**
+   * Check if the year is a leapyear
+   * @param boolean $timeStamp
+   */
+  public static function isLeapYear( $timeStamp )
+  {
+    
+    if( $timeStamp > 3000 )
+      return (date( 'L', $timeStamp ) == '1') ;
+    else 
+      return (date( 'L', mktime(  0, 0, 0, 0, 0, $timeStamp ) ) == '1');
+    
+  }//end public static function isLeapYear */
+  
+  /**
+   * 
+   */
+  public function getFilteredMonthDays( $year, $month, $days = array(), $weeks = array() )
+  {
+    
+    ///TODO wochentag donnerstag sonst keine 4 wochen etc
+    $filteredDays = array();
+    
+    $week = 1;
+    $numDays = date( 't', mktime(  0, 0, 0, 0, $month, $year ) );
+    
+    for( $pos = 1; $pos < $numDays; ++$pos )
+    {
+      
+      $theTime = mktime( 0, 0, 0, $pos, $month, $year  );
+      
+      $numDay = date('w',$theTime);
+      
+      if( in_array($numDay, $days) )
+        $filteredDays[] = $pos;
+
+      
+    }
+
+  }//end public function getFilteredMonthDays */
+  
+  /**
+   * Check if the year is a leapyear
+   * @param int $year
+   * @param int $month
+   */
+  public static function getMonthDays( $year, $month )
+  {
+    
+    return date( 't', mktime(  0, 0, 0, 0, $month, $year ) );
+      
+  }//end public static function getMonthDays */
 
 }// end class SDate
 
