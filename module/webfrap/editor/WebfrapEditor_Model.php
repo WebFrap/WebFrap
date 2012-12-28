341b09bce9d331fea8867353654e5904194e8277
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-28 11:54:28 +0100

    added base classes for the workspace editor

---
+++ module/webfrap/editor/WebfrapEditor_Model.php
@@ -0,0 +1,38 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+/**
+ * @package  WebFrap
+ * @subpackage  Core
+ * @author  Dominik  Bonsch  <dominik.bonsch@webfrap.net>
+ * @copyright  Webfrap  Developer  Network  <contact@webfrap.net>
+ * @licence  BSD
+ */
+class WebfrapEditor_Model 
+  extends Model
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//  Attributes
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+}//end  class  WebfrapEditor_Model
+
+
