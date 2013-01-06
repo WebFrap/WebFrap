405159818bc99c8ca020471e6b8711789b302579
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-01-06 15:29:56 +0100

    removed needless whitespace

--- src/wgt/WgtOverlay.php
+++ src/wgt/WgtOverlay.php
@@ -8,7 +8,7 @@
 * @projectUrl  : http://webfrap.net
 *
 * @licence     : BSD License see: LICENCE/BSD Licence.txt
-* 
+*
 * @version: @package_version@  Revision: @package_revision@
 *
 * Changes:
@@ -25,17 +25,17 @@ class WgtOverlay
 ////////////////////////////////////////////////////////////////////////////////
 // Attributes
 ////////////////////////////////////////////////////////////////////////////////
-  
+
   public $id = null;
-  
+
   public $content = null;
-  
+
   public $tooltip = null;
-  
+
   public $icon = null;
-  
+
   public $label = null;
-  
+
   /**
    * Render des Javascripts
    * @param WgtMaintab $view
@@ -43,32 +43,32 @@ class WgtOverlay
    */
   public function render( $view, $showLabel = true, $menu = null )
   {
-    
+
     $icon = '';
     if( $this->icon )
       $icon = $view->icon( $this->icon, $this->label );
-      
+
     $label = '';
     if( $this->label && $showLabel )
       $label = $this->label;
-    
+
     $html = <<<HTML
 
     <button
         class="wcm wcm_ui_dropform wcm_ui_tip-top wgt-button ui-state-default"
-      	tabindex="-1" 
+      	tabindex="-1"
         id="{$this->id}"
         tooltip="{$this->tooltip}"
       >{$icon}{$label}</button>
-      
+
     <div class="{$this->id} hidden" >
       {$this->content}
     </div>
 
 HTML;
-    
+
     return $html;
-    
+
   }//end public function renderActions */
 
 
