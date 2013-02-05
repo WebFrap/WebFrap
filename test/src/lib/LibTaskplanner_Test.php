4ca55954c8fdd80def7535749e4a171bb4b8410f
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-05 15:44:11 +0100

    added testclass for single tasks

---
+++ test/src/lib/LibTaskplanner_Test.php
@@ -0,0 +1,52 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+
+
+/**
+ * @package WebFrapUnit
+ * @subpackage WebFrap
+ */
+class LibTaskplanner_Test
+  extends LibTestUnit
+{
+
+  /**
+   * (non-PHPdoc)
+   * @see src/lib/test/LibTestUnit::setUp()
+   */
+  public function setUp()
+  {
+
+  }//end public function setUp */
+
+/*//////////////////////////////////////////////////////////////////////////////
+// access checks
+//////////////////////////////////////////////////////////////////////////////*/
+
+  /**
+   * Single Tasks finden
+   */
+  public function test_findSingleTask()
+  {
+
+
+  }//end public function test_SimpleTask */
+
+
+} //end abstract class LibTaskplanner_Test
+
