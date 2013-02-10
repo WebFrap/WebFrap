cce83b32cf8a14ac56c7588da679fb8b92113074
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-10 13:34:50 +0100

    Action: added default constructor

--- src/Action.php
+++ src/Action.php
@@ -8,7 +8,7 @@
 * @projectUrl  : http://webfrap.net
 *
 * @licence     : BSD License see: LICENCE/BSD Licence.txt
-* 
+*
 * @version: @package_version@  Revision: @package_revision@
 *
 * Changes:
@@ -33,68 +33,80 @@ class Action
    * @var array
    */
   public static $pool = array();
-  
+
   /**
    * Array mit geladenen models
    * @var array
    */
   protected $models         = array();
-  
+
 ////////////////////////////////////////////////////////////////////////////////
 // pool logic
 ////////////////////////////////////////////////////////////////////////////////
-  
+
   /**
-   * 
+   *
+   * Enter description here ...
+   * @param unknown_type $env
+   */
+  public function __construct ($env)
+  {
+
+    $this->env = $env;
+
+  }//end public function __construct */
+
+  /**
+   *
    * @param string $key
    * @param string $classname
-   * 
+   *
    * @throws Lib_Exception
    */
   public static function getActionContainer( $key, $classname )
   {
-    
+
     if( !isset( self::$pool[$key] ) )
     {
       if( !Webfrap::classLoadable( $classname ) )
       {
         throw new LibAction_Exception( 'Requested nonexisting Action: '.$classname.' key '.$key );
       }
-      else 
+      else
       {
         self::$pool[$key] = new $classname( WebFrap::$env );
       }
     }
-    
+
     return self::$pool[$key];
-    
+
   }//end public static function getActionContainer */
-  
+
   /**
    * Check ob es eine valide action ist
-   * 
+   *
    * Könnte bei den Actions öfters vorkommen, dass diese nicht existieren, bzw.
    * eine Methode aufgerufen wird welche nicht unterstützt wird
-   * 
+   *
    * @param Action $actionObj
    * @param string $methodName
-   * 
+   *
    * @return boolean
    */
   public static function check( $actionObj, $methodName )
   {
-    
+
     if( !$actionObj )
       return false;
-      
+
     // ok ist ein action object und unterstützt die methode
     if( is_object( $actionObj ) && method_exists($actionObj, $methodName) )
       return true;
-    else 
+    else
       return false;
-    
+
   }//end public static function getActionContainer */
-  
+
   /**
    * @param string $key
    * @param Model $model
@@ -103,26 +115,26 @@ class Action
   {
     $this->models[$key] = $model;
   }//end public function setModel */
-    
+
   /**
    * @param string $key
    * @return Model
    */
   public function getModel( $key )
   {
-    if( isset( $this->models[$key] ) ) 
+    if( isset( $this->models[$key] ) )
       return $this->models[$key];
     else
       return null;
-      
+
   }//end public function setModel */
-  
+
   /**
    * Eine Modelklasse laden
-   * 
+   *
    * @param string $modelKey
    * @param string $key
-   * 
+   *
    * @return Model
    * @throws ModelNotExists_Exception wenn das angefragt Modell nicht existiert
    */
@@ -139,7 +151,7 @@ class Action
       if( Webfrap::classLoadable( $modelName ) )
       {
         $model = new $modelName( $this );
-        
+
         $this->models[$key] = $model;
       }
       else
@@ -155,18 +167,18 @@ class Action
     return $this->models[$key];
 
   }//end public function loadModel */
-  
+
 ////////////////////////////////////////////////////////////////////////////////
 // default method
 ////////////////////////////////////////////////////////////////////////////////
-  
+
   /**
    * @param Entity $entity
    * @param Base $caller
    * /
   public function trigger( $entity, $caller )
   {
-    
+
   }//end public function trigger */
 
 }// end class Action
