1127af4a9bf612d9d31ee0044b9b8a622abe2abe
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-14 10:32:27 +0100

    convention

--- src/controller/ControllerRouter.php
+++ src/controller/ControllerRouter.php
@@ -1,20 +1,20 @@
 <?php
-/*******************************************************************************
-*
-* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
-* @date        :
-* @copyright   : Webfrap Developer Network <contact@webfrap.net>
-* @project     : Webfrap Web Frame Application
-* @projectUrl  : http://webfrap.net
-*
-* @licence     : BSD License see: LICENCE/BSD Licence.txt
-* 
-* @version: @package_version@  Revision: @package_revision@
-*
-* Changes:
-*
-*******************************************************************************/
 
+/*******************************************************************************
+ *
+ * @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @date        :
+ * @copyright   : Webfrap Developer Network <contact@webfrap.net>
+ * @project     : Webfrap Web Frame Application
+ * @projectUrl  : http://webfrap.net
+ *
+ * @licence     : BSD License see: LICENCE/BSD Licence.txt
+ * 
+ * @version: @package_version@  Revision: @package_revision@
+ *
+ * Changes:
+ *
+ *******************************************************************************/
 
 /**
  * Routing Controller mit dessen Hilfe Anfragen je nach Eigenschaft 
@@ -25,120 +25,92 @@
  * @author dominik alexander bonsch <dominik.bonsch@webfrap.net>
  *
  */
-class ControllerRouter
-  extends ControllerCrud
+class ControllerRouter extends ControllerCrud
 {
-  
+
   /**
    * @param string $action
    */
-  public function getRouteName( $action )
+  public function getRouteName ($action)
   {
-    
+
     return null;
-    
-  }//end public function getRouteName */
+  
+  } //end public function getRouteName */
+
   
   /**
    * die vom request angeforderte methode auf rufen
    * 
    * @param string $action
    */
-  public function run( $action = null )
+  public function run ($action = null)
   {
+
+    $controllerName = $this->getRouteName($action);
     
-    $controllerName = $this->getRouteName( $action );
-    
-    if( !$controllerName )
-    {
-      $this->errorPage
-      (
-        "Found no Route for your request",
-        Response::NOT_FOUND
-      );
+    if (! $controllerName) {
+      $this->errorPage("Found no Route for your request", Response::NOT_FOUND);
       return false;
     }
     
-    $className = $controllerName.'_Controller';
-
-    if( WebFrap::loadable( $className ) )
-    {
-      $controller = new $className( );
-    }
-    else
-    {
-
-      $this->errorPage
-      (
-        "Invalid Route, the target ".$controllerName.' not exists.',
-        Response::INTERNAL_ERROR
-      );
-      return false;
-    }
+    $className = $controllerName . '_Controller';
     
-    
-    try 
-    {
-    
-      // Initialisieren der Extention
-      if( !$controller->initController( ) )
-        throw new Webfrap_Exception( 'Failed to initialize Controller' );
-
-      // Run the mainpart
-      $controller->run( $action );
-
-      // shout down the extension
-      $controller->shutdownController( );
-
-    }
-    catch( Exception $exc )
-    {
-
-      $type = get_class($exc);
-
-      if( Log::$levelDebug )
-      {
-        // Create a Error Page
-        $this->errorPage
-        (
-          $exc->getMessage(),
-          Response::INTERNAL_ERROR,
-          $exc
-        );
-
-      }
-      else
-      {
-        switch( $type )
-        {
-          case 'Security_Exception':
-          {
-            $this->errorPage
-            (
-              $response->i18n->l( 'Access Denied', 'wbf.message'  ),
-              Error::NOT_AUTHORIZED
-            );
-            
-            break;
-          }
-          default:
-          {
-
-            $this->errorPage
-            (
-              $response->i18n->l(  'Sorry Internal Error', 'wbf.message'  ),
-              Response::INTERNAL_ERROR
-            );
-
-            break;
-          }//end default
-
-        }//end switch 
-
-      }//end else
-
-    }//end catch 
+    if (WebFrap::loadable($className)) {
+      $controller = new $className();
+    } else {
       
-  }//end public function run */
+      $this->errorPage("Invalid Route, the target " . $controllerName . ' not exists.', Response::INTERNAL_ERROR);
+      return false;
+    }
+    
+    try {
+      
+      // Initialisieren der Extention
+      if (! $controller->initController())
+        throw new Webfrap_Exception('Failed to initialize Controller');
+      
+   // Run the mainpart
+      $controller->run($action);
+      
+      // shout down the extension
+      $controller->shutdownController();
+    
+    } catch (Exception $exc) {
+      
+      $type = get_class($exc);
+      
+      if (Log::$levelDebug) {
+        // Create a Error Page
+        $this->errorPage($exc->getMessage(), Response::INTERNAL_ERROR, $exc);
+      
+      } else {
+        switch ($type) {
+          case 'Security_Exception' :
+            {
+              $this->errorPage($response->i18n->l('Access Denied', 'wbf.message'), Error::NOT_AUTHORIZED);
+              
+              break;
+            }
+          default :
+            {
+              
+              $this->errorPage($response->i18n->l('Sorry Internal Error', 'wbf.message'), Response::INTERNAL_ERROR);
+              
+              break;
+            } //end default
+        
+
+        } //end switch 
+      
+
+      } //end else
+    
+
+    } //end catch 
+  
+
+  } //end public function run */
+
 
 } // end class ControllerRouter
