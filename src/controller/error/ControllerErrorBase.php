f2c0cf56a55475c40e501a8b95aa7d73c45021f8
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-14 10:31:04 +0100

    convention

--- src/controller/error/ControllerErrorBase.php
+++ src/controller/error/ControllerErrorBase.php
@@ -1,20 +1,20 @@
 <?php
-/*******************************************************************************
-*
-* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
-* @date        :
-* @copyright   : Webfrap Developer Network <contact@webfrap.net>
-* @project     : Webfrap Web Frame Application
-* @projectUrl  : http://webfrap.net
-*
-* @licence     : BSD License see: LICENCE/BSD Licence.txt
-* 
-* @version: @package_version@  Revision: @package_revision@
-*
-* Changes:
-*
-*******************************************************************************/
 
+/*******************************************************************************
+ *
+ * @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @date        :
+ * @copyright   : Webfrap Developer Network <contact@webfrap.net>
+ * @project     : Webfrap Web Frame Application
+ * @projectUrl  : http://webfrap.net
+ *
+ * @licence     : BSD License see: LICENCE/BSD Licence.txt
+ * 
+ * @version: @package_version@  Revision: @package_revision@
+ *
+ * Changes:
+ *
+ *******************************************************************************/
 
 /**
  * Basisklasse für Error hanlder Controller
@@ -22,37 +22,36 @@
  * @package WebFrap
  * @subpackage tech_core
  */
-class ControllerErrorBase
-  extends Controller
+class ControllerErrorBase extends Controller
 {
 
 /*//////////////////////////////////////////////////////////////////////////////
 // Der Controller
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @param string $action
    */
-  public function run( $action = null )
+  public function run ($action = null)
   {
 
     $this->showErrorPage();
+  
+  } //end public function run
 
-  }//end public function run
-
-
+  
 /*//////////////////////////////////////////////////////////////////////////////
 // Methoden
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @return void
    */
-  public function showErrorPage( )
+  public function showErrorPage ()
   {
 
-    $this->view->setTemplate( 'ModError' , 'errors' );
-
+    $this->view->setTemplate('ModError', 'errors');
+  
   } // end public function errorPage
 
 
