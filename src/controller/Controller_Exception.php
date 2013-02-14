11a342696a51d29b81d782edc79be7c13693359e
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-14 10:31:16 +0100

    convention

--- src/controller/Controller_Exception.php
+++ src/controller/Controller_Exception.php
@@ -1,20 +1,20 @@
 <?php
-/*******************************************************************************
-*
-* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
-* @date        :
-* @copyright   : Webfrap Developer Network <contact@webfrap.net>
-* @project     : Webfrap Web Frame Application
-* @projectUrl  : http://webfrap.net
-*
-* @licence     : BSD License see: LICENCE/BSD Licence.txt
-* 
-* @version: @package_version@  Revision: @package_revision@
-*
-* Changes:
-*
-*******************************************************************************/
 
+/*******************************************************************************
+ *
+ * @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @date        :
+ * @copyright   : Webfrap Developer Network <contact@webfrap.net>
+ * @project     : Webfrap Web Frame Application
+ * @projectUrl  : http://webfrap.net
+ *
+ * @licence     : BSD License see: LICENCE/BSD Licence.txt
+ * 
+ * @version: @package_version@  Revision: @package_revision@
+ *
+ * Changes:
+ *
+ *******************************************************************************/
 
 /**
  * Exception to throw if you want to throw an unspecific Exception inside the
@@ -25,8 +25,7 @@
  * @package WebFrap
  * @subpackage tech_core
  */
-class Controller_Exception
-  extends WebfrapSys_Exception
+class Controller_Exception extends WebfrapSys_Exception
 {
 
 } // end class Controller_Exception
