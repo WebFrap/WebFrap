21d6d90e4a1f01eb66af1660fef0121f0b5a2fb3
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-15 16:22:05 +0100

    code review, apidoc fixed & code convention

--- src/Acl.php
+++ src/Acl.php
@@ -1,33 +1,31 @@
 <?php
 /*******************************************************************************
-*
-* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
-* @date        :
-* @copyright   : Webfrap Developer Network <contact@webfrap.net>
-* @project     : Webfrap Web Frame Application
-* @projectUrl  : http://webfrap.net
-*
-* @licence     : BSD License see: LICENCE/BSD Licence.txt
-*
-* @version: @package_version@  Revision: @package_revision@
-*
-* Changes:
-*
-*******************************************************************************/
+ *
+ * @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @date        :
+ * @copyright   : Webfrap Developer Network <contact@webfrap.net>
+ * @project     : Webfrap Web Frame Application
+ * @projectUrl  : http://webfrap.net
+ *
+ * @licence     : BSD License see: LICENCE/BSD Licence.txt
+ *
+ * @version: @package_version@  Revision: @package_revision@
+ *
+ * Changes:
+ *
+ *******************************************************************************/
 
+if (! defined('ACL_ASSIGNED_SOURCE'))
+  define('ACL_ASSIGNED_SOURCE', 'webfrap_acl_assigned_view');
 
-if (!defined( 'ACL_ASSIGNED_SOURCE' ) )
-  define( 'ACL_ASSIGNED_SOURCE', 'webfrap_acl_assigned_view' );
+if (! defined('ACL_MAX_PERMISSION'))
+  define('ACL_MAX_PERMISSION', 'webfrap_acl_max_permission_view');
 
-if (!defined( 'ACL_MAX_PERMISSION' ) )
-  define( 'ACL_MAX_PERMISSION', 'webfrap_acl_max_permission_view' );
+if (! defined('ACL_RELATION'))
+  define('ACL_RELATION', 'webfrap_inject_acls_view');
 
-if (!defined( 'ACL_RELATION' ) )
-  define( 'ACL_RELATION', 'webfrap_inject_acls_view' );
-
-if (!defined( 'ACL_ROLE_RELATION' ) )
-  define( 'ACL_ROLE_RELATION', 'webfrap_has_arearole_view' );
-
+if (! defined('ACL_ROLE_RELATION'))
+  define('ACL_ROLE_RELATION', 'webfrap_has_arearole_view');
 
 /**
  * @lang de:
@@ -48,10 +46,11 @@ if (!defined( 'ACL_ROLE_RELATION' ) )
  */
 class Acl
 {
-/*//////////////////////////////////////////////////////////////////////////////
+
+  /*//////////////////////////////////////////////////////////////////////////////
 // Constantes
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @lang de:
    *
@@ -61,10 +60,9 @@ class Acl
    */
   const DENIED = 0;
 
-
   /**
    * @lang de:
-   *   Darf Elemente auflisten
+   * Darf Elemente auflisten
    * }
    * @var int
    */
@@ -72,8 +70,8 @@ class Acl
 
   /**
    * @lang de:
-   *   Lesene operationen sind gestatten, es dürfen jedoch keine änderungen
-   *   gemacht bzw übernommen werden
+   * Lesene operationen sind gestatten, es dürfen jedoch keine änderungen
+   * gemacht bzw übernommen werden
    * }
    * @var int
    */
@@ -81,7 +79,7 @@ class Acl
 
   /**
    * @lang de:
-   *   der benuzter darf referenzen auf diesen datensatz erstellen
+   * der benuzter darf referenzen auf diesen datensatz erstellen
    * }
    * @var int
    */
@@ -89,7 +87,7 @@ class Acl
 
   /**
    * @lang de:
-   *   Der Benutzer darf zusätzlich neuen Datensätze anlegen
+   * Der Benutzer darf zusätzlich neuen Datensätze anlegen
    * }
    * @var int
    */
@@ -97,7 +95,7 @@ class Acl
 
   /**
    * @lang de:
-   *   Der Benutzer darf bestehend Datensätze verändern
+   * Der Benutzer darf bestehend Datensätze verändern
    * }
    * @var int
    */
@@ -105,7 +103,7 @@ class Acl
 
   /**
    * @lang de:
-   *   der benutzer darf löschen
+   * der benutzer darf löschen
    * }
    * @var int
    */
@@ -113,7 +111,7 @@ class Acl
 
   /**
    * @lang de:
-   *   Der Benutzer darf Datensätze sichtbar schalten
+   * Der Benutzer darf Datensätze sichtbar schalten
    * }
    * @var int
    */
@@ -121,7 +119,7 @@ class Acl
 
   /**
    * @lang de:
-   *   der Benutzer darf metadaten verwalten
+   * der Benutzer darf metadaten verwalten
    * }
    * @var int
    */
@@ -129,8 +127,8 @@ class Acl
 
   /**
    * @lang de:
-   *   Der Benutzer darf rechte vergeben und sonstige administrative Aufgaben
-   *   durchführen
+   * Der Benutzer darf rechte vergeben und sonstige administrative Aufgaben
+   * durchführen
    * }
    * @var int
    */
@@ -144,44 +142,44 @@ class Acl
    */
   const ACL_OVERWRITE = true;
 
-/*//////////////////////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////////////////////////*/
+  
+  /**
+   * @var string
+   */
+  const ROLE_SOMEWHERE = 'somewhere';
 
   /**
    * @var string
    */
-  const ROLE_SOMEWHERE   = 'somewhere';
+  const ROLE_DATASET = 'dataset';
 
   /**
    * @var string
    */
-  const ROLE_DATASET     = 'dataset';
+  const OWNER = 'owner';
 
   /**
    * @var string
    */
-  const OWNER     = 'owner';
+  const USER = 'user';
 
   /**
    * @var string
    */
-  const USER     = 'user';
+  const ROLE = 'role';
 
   /**
    * @var string
    */
-  const ROLE     = 'role';
+  const PROFILE = 'profile';
 
-  /**
-   * @var string
-   */
-  const PROFILE     = 'profile';
-
-/*//////////////////////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @lang de:
    *
@@ -191,25 +189,24 @@ class Acl
    * auch alle rechte die einen kleineres zugriffslevel benötigen
    * @var array
    */
-  public static $accessLevels = array
-  (
-    'denied'      => 0,
-    'listing'     => 1,
-    'access'      => 2,
-    'assign'      => 4,
-    'insert'      => 8,
-    'update'      => 16,
-    'delete'      => 32,
-    'publish'     => 64,
-    'maintenance' => 128,
-    'rights'      => 256,
-    'admin'       => 256,
+  public static $accessLevels = array(
+    'denied' => 0, 
+    'listing' => 1, 
+    'access' => 2, 
+    'assign' => 4, 
+    'insert' => 8, 
+    'update' => 16, 
+    'delete' => 32, 
+    'publish' => 64, 
+    'maintenance' => 128, 
+    'rights' => 256, 
+    'admin' => 256
   );
 
-/*//////////////////////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////////////////////
 // Methodes
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @lang de:
    * da in der regel nur ein acls objekt benötigt wird, hält ACL eine referenz
@@ -218,64 +215,66 @@ class Acl
    * expliziet übergeben wurde
    * @var LibAclAdapter
    */
-  protected static $instance    = null;
+  protected static $instance = null;
 
   /**
    * Standard instance für den ACL Manager
    * @var LibAclManager
    */
-  protected static $manager    = null;
+  protected static $manager = null;
 
   /**
    * @lang de:
    * the user level
    * @var array
    */
-  protected $level              = array();
+  protected $level = array();
 
   /**
    * @var array
    */
-  protected $group              = array();
+  protected $group = array();
 
   /**
    * @var array
    */
-  protected $extend             = array();
+  protected $extend = array();
 
   /**
    * @var array
    */
-  protected $groupCache         = array();
+  protected $groupCache = array();
 
   /**
    * @var array
    */
-  protected $lists              = array();
+  protected $lists = array();
 
   /**
    * @var User
    */
-  protected $user               = null;
+  protected $user = null;
 
-/*//////////////////////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////////////////////
 // Messaging System
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * Standard Konstruktor
    *
    * Der aktive Benutzer wird geladen
    */
-  public function __construct( )
+  public function __construct()
   {
-    $this->user = User::getActive();
-  }//end public function __construct
 
-/*//////////////////////////////////////////////////////////////////////////////
+    $this->user = User::getActive();
+  } //end public function __construct
+
+  
+  /*//////////////////////////////////////////////////////////////////////////////
 // getter & setter
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * Init Methode
    * Diese Methode wird im bootstrap aufgerufen und erzeugt eine Standard
@@ -288,35 +287,35 @@ class Acl
    *
    * @return void
    */
-  public static function init($env )
+  public static function init($env)
   {
 
-    if ( self::$instance )
+    if (self::$instance)
       return;
-
-    if (!defined('WBF_ACL_ADAPTER') )
-    {
-      self::$instance = new LibAclAdapter_Db($env );
-
+    
+    if (! defined('WBF_ACL_ADAPTER')) {
+      self::$instance = new LibAclAdapter_Db($env);
+      
       // mit der WBF_NO_ACL Konstante kann ein überprüfen der rechte unterbunden werden
-      if ( defined('WBF_NO_ACL') && WBF_NO_ACL )
-        self::$instance->setDisabled( true );
-
+      if (defined('WBF_NO_ACL') && WBF_NO_ACL)
+        self::$instance->setDisabled(true);
+      
       return;
     }
-
-    $className = 'LibAclAdapter_'.ucfirst(WBF_ACL_ADAPTER);
-
-    self::$instance = new $className($env );
-
+    
+    $className = 'LibAclAdapter_' . ucfirst(WBF_ACL_ADAPTER);
+    
+    self::$instance = new $className($env);
+    
     // mit der WBF_NO_ACL Konstante kann ein überprüfen der rechte unterbunden werden
-    if ( defined('WBF_NO_ACL') && WBF_NO_ACL )
-      self::$instance->setDisabled( true );
-
+    if (defined('WBF_NO_ACL') && WBF_NO_ACL)
+      self::$instance->setDisabled(true);
+    
     return;
+  
+  } //end public static function init */
 
-  }//end public static function init */
-
+  
   /**
    * statische getter für das standard acl objekt
    *
@@ -327,21 +326,21 @@ class Acl
    *
    * @return LibAclDb
    */
-  public static function getActive($env = null )
+  public static function getActive($env = null)
   {
 
-    if (!self::$instance )
-    {
-      if (!$env )
+    if (! self::$instance) {
+      if (! $env)
         $env = Webfrap::getActive();
-
-      self::init($env );
+      
+      self::init($env);
     }
-
+    
     return self::$instance;
+  
+  } //end public static function getActive
 
-  }//end public static function getActive
-
+  
   /**
    * statische getter für das standard acl objekt
    *
@@ -352,63 +351,60 @@ class Acl
    *
    * @return LibAclDb
    */
-  public static function getManager($env = null )
+  public static function getManager($env = null)
   {
 
-    if (!self::$manager )
-    {
-      if (!$env )
+    if (! self::$manager) {
+      if (! $env)
         $env = Webfrap::getActive();
-
-      if (!defined('WBF_ACL_ADAPTER') )
-      {
-        self::$manager = new LibAclManager_Db($env );
+      
+      if (! defined('WBF_ACL_ADAPTER')) {
+        self::$manager = new LibAclManager_Db($env);
       } else {
-        $className = 'LibAclManager_'.ucfirst(WBF_ACL_ADAPTER);
-        self::$manager = new $className($env );
+        $className = 'LibAclManager_' . ucfirst(WBF_ACL_ADAPTER);
+        self::$manager = new $className($env);
       }
     }
-
+    
     return self::$manager;
+  
+  } //end public static function getActive
 
-  }//end public static function getActive
-
-
+  
   /**
    *
    * @param User $user
    */
-  public function setUser($user )
+  public function setUser($user)
   {
-    $this->user = $user;
-  }//end public function setUser
 
-/*//////////////////////////////////////////////////////////////////////////////
+    $this->user = $user;
+  } //end public function setUser
+
+  
+  /*//////////////////////////////////////////////////////////////////////////////
 // Messaging System
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * @param string $key
    */
-  public function loadLists($key )
+  public function loadLists($key)
   {
 
-    if ( isset($this->lists[$key]) )
+    if (isset($this->lists[$key]))
       return false;
-
+    
     $path = null;
-
-    foreach( Conf::$confPath as $rootPath )
-    {
-      if ( file_exists($rootPath.'acl/'.$key.'.acl.php' ) )
-      {
-        $path = $rootPath.'acl/'.$key.'.acl.php';
+    
+    foreach (Conf::$confPath as $rootPath) {
+      if (file_exists($rootPath . 'acl/' . $key . '.acl.php')) {
+        $path = $rootPath . 'acl/' . $key . '.acl.php';
         break;
       }
     }
-
-    if ($path )
-    {
+    
+    if ($path) {
       include $path;
       $this->lists[$key] = true;
       return true;
@@ -416,166 +412,164 @@ class Acl
       $this->lists[$key] = false;
       return false;
     }
+  
+  } //end public function loadLists
 
-  }//end public function loadLists
-
+  
   /**
    *
    * @param string $key
    * @param string $access
    * @return boolean
    */
-  public function level($key , $access = null )
+  public function level($key, $access = null)
   {
 
-    $tmp     = explode(':',$key);
-    $orgKey  = $tmp[0];
-    $files   = explode( '/' , $tmp[0] ) ;
-    $key     = $tmp[1];
-
-    if (is_null($access ) )
+    $tmp = explode(':', $key);
+    $orgKey = $tmp[0];
+    $files = explode('/', $tmp[0]);
+    $key = $tmp[1];
+    
+    if (is_null($access))
       $access = $this->user->getLevel();
-
+    
     $fullKey = array();
-
-    foreach($files as $subPath )
-    {
-
+    
+    foreach ($files as $subPath) {
+      
       $fullKey[] = $subPath;
-      $file = implode('/',$fullKey);
-
-      if (!isset($this->level[$file][$key] ) )
-        if (!$this->loadLists($file ) )
-          if (!$this->checkLevelExtend($file, $key, $access )  && $orgKey == $file )
+      $file = implode('/', $fullKey);
+      
+      if (! isset($this->level[$file][$key]))
+        if (! $this->loadLists($file))
+          if (! $this->checkLevelExtend($file, $key, $access) && $orgKey == $file)
             return false;
-
-      if ( isset($this->level[$file][$key] )  )
-      {
-        if ($this->level[$file][$key] <= $access )
+      
+      if (isset($this->level[$file][$key])) {
+        if ($this->level[$file][$key] <= $access)
           return true;
-      }
-      else if ($this->checkLevelExtend($file, $key , $access  ) )
-      {
+      } else if ($this->checkLevelExtend($file, $key, $access)) {
         return true;
       }
-
-    }//end foreach
+    
+    } //end foreach
+    
 
     return false;
+  
+  } //end public function level
 
-  }//end public function level
-
+  
   /**
    *
    * @param string $key
    * @param string $access
    * @return boolean
    */
-  public function group($key , $access = null )
+  public function group($key, $access = null)
   {
 
-    $tmp    = explode(':',$key);
+    $tmp = explode(':', $key);
     $orgKey = $tmp[0];
-    $files  = explode( '/' , $tmp[0] ) ;
-    $key    = $tmp[1];
-
-    if (is_null($access) )
+    $files = explode('/', $tmp[0]);
+    $key = $tmp[1];
+    
+    if (is_null($access))
       $access = $this->user->getGroups();
-
-    // check all parentareas and the given area if the rights are valid
-    foreach($files as $subPath )
-    {
-
+    
+   // check all parentareas and the given area if the rights are valid
+    foreach ($files as $subPath) {
+      
       $fullKey[] = $subPath;
-      $file = implode('/',$fullKey);
-
-      if (!isset($this->group[$file][$key]) )
-      {
+      $file = implode('/', $fullKey);
+      
+      if (! isset($this->group[$file][$key])) {
         // if this is the original Path an there are no ALCs access ist denied
-        if (!$this->loadLists($file) && $orgKey == $file )
+        if (! $this->loadLists($file) && $orgKey == $file)
           return false;
       }
-
+      
       //if there are no groupdata end we are in the last file finish here
-      if (!isset($this->group[$file][$key]) && !is_array($this->group[$file][$key]) && $orgKey == $file )
+      if (! isset($this->group[$file][$key]) && ! is_array($this->group[$file][$key]) && $orgKey == $file)
         return false;
-
-      foreach($access as $role )
-      {
-        if ( in_array($role, $this->group[$file][$key] ) )
+      
+      foreach ($access as $role) {
+        if (in_array($role, $this->group[$file][$key]))
           return true;
-        else  if ($this->checkGroupExtend($file, $key , $access  ) )
+        else if ($this->checkGroupExtend($file, $key, $access))
           return true;
       }
-
-    }//end foreach
+    
+    } //end foreach
+    
 
     // Wenn nichts gefunden wurde dann ist es automatisch falsch
     //$this->groupCache[$tmp[0]][$tmp[1]] = false;
     return false;
+  
+  } //end public function group
 
-  }//end public function group
-
-
+  
   /**
-   *
    * @param string $key
+   * @param string $entity
    * @return unknown_type
    */
-  public function permission($key, $entity = null  )
+  public function permission($key, $entity = null)
   {
 
-    if ( defined( 'WBF_NO_ACL' ) )
+    if (defined('WBF_NO_ACL'))
       return true;
-
-    if ($this->user->getLevel() >= User::LEVEL_FULL_ACCESS )
+    
+    if ($this->user->getLevel() >= User::LEVEL_FULL_ACCESS)
       return true;
-
-    if ( is_array($key) )
-    {
-
-      foreach($key as $tmpKey )
-      {
-        if ($this->level($tmpKey) )
+    
+    if (is_array($key)) {
+      
+      foreach ($key as $tmpKey) {
+        if ($this->level($tmpKey))
           return true;
-
-        if ($this->group($tmpKey) )
+        
+        if ($this->group($tmpKey))
           return true;
       }
-
+    
     } else {
-      if ($this->level($key) )
+      if ($this->level($key))
         return true;
-
-      if ($this->group($key) )
+      
+      if ($this->group($key))
         return true;
     }
-
+    
     return false;
+  
+  } //end public function permission
 
-  }//end public function permission
-
+  
   /**
    *
    * @param $file
    * @param $key
    * @param $level
+   * 
    * @return boolean
    */
-  public function checkLevelExtend($file, $key, $level )
+  public function checkLevelExtend($file, $key, $level)
   {
 
-    if (!isset($this->extend[$file][$key] ))
+    if (! isset($this->extend[$file][$key]))
       return false;
-
-    foreach($this->extend[$file][$key] as $key )
-      if ($this->level($key, $level ) )
+    
+    foreach ($this->extend[$file][$key] as $key)
+      if ($this->level($key, $level))
         return true;
-
+    
     return false;
+  
+  } //end public function checkLevelExtend
 
-  }//end public function checkLevelExtend
-
+  
   /**
    *
    * @param $path
@@ -583,32 +577,31 @@ class Acl
    * @param $groups
    * @return unknown_type
    */
-  public function checkGroupExtend($path , $key , $groups )
+  public function checkGroupExtend($path, $key, $groups)
   {
-    if (!isset($this->extend[$path][$key] ))
+
+    if (! isset($this->extend[$path][$key]))
       return false;
-
-    foreach($this->extend[$path][$key] as $extKey )
-      if ($this->group($extKey , $groups ) )
+    
+    foreach ($this->extend[$path][$key] as $extKey)
+      if ($this->group($extKey, $groups))
         return true;
-
+    
     return false;
-  }//end public function checkGroupExtend
+  } //end public function checkGroupExtend
 
+  
   /**
-   *
-   * @param $key
-   * @param $access
-   * @return boolean
    */
-  public function debug( )
+  public function debug()
   {
 
-    Debug::console('$this->level',$this->level);
-    Debug::console('$this->group',$this->group);
-    Debug::console('$this->extend',$this->extend);
+    Debug::console('$this->level', $this->level);
+    Debug::console('$this->group', $this->group);
+    Debug::console('$this->extend', $this->extend);
+  
+  } //end public function debug
 
-  }//end public function debug
 
 }//end class Acl
 
