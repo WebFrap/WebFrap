91d0fe3570e26a1e2b01e985de54abb0bf439099
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-15 16:23:14 +0100

    codereview, apidoc fixed, codeconvention

--- src/Action.php
+++ src/Action.php
@@ -1,19 +1,20 @@
 <?php
+
 /*******************************************************************************
-*
-* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
-* @date        :
-* @copyright   : Webfrap Developer Network <contact@webfrap.net>
-* @project     : Webfrap Web Frame Application
-* @projectUrl  : http://webfrap.net
-*
-* @licence     : BSD License see: LICENCE/BSD Licence.txt
-*
-* @version: @package_version@  Revision: @package_revision@
-*
-* Changes:
-*
-*******************************************************************************/
+ *
+ * @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @date        :
+ * @copyright   : Webfrap Developer Network <contact@webfrap.net>
+ * @project     : Webfrap Web Frame Application
+ * @projectUrl  : http://webfrap.net
+ *
+ * @licence     : BSD License see: LICENCE/BSD Licence.txt
+ *
+ * @version: @package_version@  Revision: @package_revision@
+ *
+ * Changes:
+ *
+ *******************************************************************************/
 
 /**
  * Static Interface to get the activ configuration object
@@ -23,10 +24,11 @@
  */
 class Action extends BaseChild
 {
+
 /*//////////////////////////////////////////////////////////////////////////////
 // static attributes
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
    * Action Pool
    * @var array
@@ -42,45 +44,44 @@ class Action extends BaseChild
 /*//////////////////////////////////////////////////////////////////////////////
 // pool logic
 //////////////////////////////////////////////////////////////////////////////*/
-
+  
   /**
-   *
-   * Enter description here ...
    * @param unknown_type $env
    */
-  public function __construct ($env)
+  public function __construct($env)
   {
 
     $this->env = $env;
+  
+  } //end public function __construct */
 
-  }//end public function __construct */
-
+  
   /**
-   *
    * @param string $key
    * @param string $classname
    *
    * @throws Lib_Exception
    */
-  public static function getActionContainer ($key, $classname)
+  public static function getActionContainer($key, $classname)
   {
 
-    if (!isset(self::$pool[$key])) {
-
-      if (!Webfrap::classLoadable($classname)) {
-
-        throw new LibAction_Exception('Requested nonexisting Action: '.$classname.' key '.$key);
-
+    if (! isset(self::$pool[$key])) {
+      
+      if (! Webfrap::classLoadable($classname)) {
+        
+        throw new LibAction_Exception('Requested nonexisting Action: ' . $classname . ' key ' . $key);
+      
       } else {
-
+        
         self::$pool[$key] = new $classname(WebFrap::$env);
       }
     }
-
+    
     return self::$pool[$key];
+  
+  } //end public static function getActionContainer */
 
-  }//end public static function getActionContainer */
-
+  
   /**
    * Check ob es eine valide action ist
    *
@@ -95,39 +96,44 @@ class Action extends BaseChild
   public static function check($actionObj, $methodName)
   {
 
-    if (!$actionObj )
+    if (! $actionObj)
       return false;
-
-    // ok ist ein action object und unterstützt die methode
-    if ( is_object($actionObj ) && method_exists($actionObj, $methodName) )
+    
+   // ok ist ein action object und unterstützt die methode
+    if (is_object($actionObj) && method_exists($actionObj, $methodName))
       return true;
     else
       return false;
+  
+  } //end public static function getActionContainer */
 
-  }//end public static function getActionContainer */
-
+  
   /**
    * @param string $key
    * @param Model $model
    */
-  public function setModel($key, $model )
+  public function setModel($key, $model)
   {
-    $this->models[$key] = $model;
-  }//end public function setModel */
 
+    $this->models[$key] = $model;
+  } //end public function setModel */
+
+  
   /**
    * @param string $key
    * @return Model
    */
-  public function getModel($key )
+  public function getModel($key)
   {
-    if ( isset($this->models[$key] ) )
+
+    if (isset($this->models[$key]))
       return $this->models[$key];
     else
       return null;
+  
+  } //end public function setModel */
 
-  }//end public function setModel */
-
+  
   /**
    * Eine Modelklasse laden
    *
@@ -137,45 +143,40 @@ class Action extends BaseChild
    * @return Model
    * @throws ModelNotExists_Exception wenn das angefragt Modell nicht existiert
    */
-  public function loadModel($modelKey, $key = null )
+  public function loadModel($modelKey, $key = null)
   {
 
-    if (!$key )
+    if (! $key)
       $key = $modelKey;
-
-    $modelName    = $modelKey.'_Model';
-
-    if (!isset($this->models[$key]  ) )
-    {
-      if ( Webfrap::classLoadable($modelName ) )
-      {
-        $model = new $modelName($this );
-
+    
+    $modelName = $modelKey . '_Model';
+    
+    if (! isset($this->models[$key])) {
+      if (Webfrap::classLoadable($modelName)) {
+        $model = new $modelName($this);
+        
         $this->models[$key] = $model;
       } else {
-        throw new ModelNotExists_Exception
-        (
-          'Internal Error',
-          'The requested model: '.$modelName.' not exists.'
-        );
+        throw new ModelNotExists_Exception('Internal Error', 'The requested model: ' . $modelName . ' not exists.');
       }
     }
-
+    
     return $this->models[$key];
+  
+  } //end public function loadModel */
 
-  }//end public function loadModel */
 
 /*//////////////////////////////////////////////////////////////////////////////
 // default method
 //////////////////////////////////////////////////////////////////////////////*/
 
-  /**
-   * @param Entity $entity
-   * @param Base $caller
-   * /
+/**
+ * @param Entity $entity
+ * @param Base $caller
+ * /
   public function trigger($entity, $caller )
   {
 
-  }//end public function trigger */
+ */
 
 }// end class Action
