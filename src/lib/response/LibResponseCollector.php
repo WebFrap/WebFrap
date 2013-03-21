69a3e5de965da026df52885f8ad318aa4b8a3d3d
johannes.dillmann <johannes.dillmann@webfrap.net>
2013-03-21 12:01:03 +0100

    skeletor

---
+++ src/lib/response/LibResponseCollector.php
@@ -0,0 +1,95 @@
+<?php
+
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+/**
+ * @package WebFrap
+ * @subpackage tech_core
+ */
+class LibResponseCollector extends LibResponse
+{
+
+  /**
+   * Puffer für ankommende Nachrichten
+   * @var array
+   */
+  public $message = array();
+
+  /**
+   * Puffer für ankommende Warnungen
+   * @var array
+   */
+  public $warning = array();
+
+  /**
+   * Puffer für ankommende Fehler
+   * @var array
+   */
+  public $error = array();
+
+  /**
+   * Puffer für alle Ereignisse die protokolliert werden
+   * @var array
+   */
+  public $protocol = array();
+  
+  public function __construct() {
+    
+  }
+
+  /**
+   * Fügt eine neue Nachricht zu den Nachrichten hinzu
+   * @param string $message
+   */
+  public function addMessage ($message)
+  {
+
+    $this->message[] = $message;
+  }
+
+  /**
+   * Fügt eine neue Warnung zu den Warnungen hinzu
+   * @param string $warning
+   */
+  public function addWarning ($warning)
+  {
+
+    $this->warning[] = $warning;
+  }
+
+  /**
+   * Fügt einen neuen Fehler zu den Fehlern hinzu
+   * @param string $error
+   */
+  public function addError ($error)
+  {
+
+    $this->error[] = $error;
+  }
+
+  /**
+   * 
+   * @param string $message
+   * @param string $context
+   * @param Entity $entity
+   * @param string $mask
+   */
+  public function protocol ($message, $context, $entity = null, $mask = null)
+  {
+
+  }
+}
