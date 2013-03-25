eb942782309309c54650400fd2b54b58459a8259
johannes.dillmann <johannes.dillmann@webfrap.net>
2013-03-25 11:32:54 +0100

    use response object

--- module/backup/Backup_Action.php
+++ module/backup/Backup_Action.php
@@ -1,120 +1,145 @@
 <?php
-class Backup_Action extends Action {
-	
-	/**
+
+class Backup_Action extends Action
+{
+
+  /**
 	 * Benutzerobjekt mit dessen Rechten die Aktion ausgeführt wird.
 	 *
 	 * @var User
 	 */
-	public $user = null;
-	
-	/**
+  public $user = null;
+
+  /**
 	 * Das Umgebungsobjekt.
 	 *
 	 * @var LibFlowApachemod $env
 	 */
-	public $env = null;
-	
-	/**
+  public $env = null;
+
+  /**
 	 * Pfad zu den Backups
 	 *
 	 * @var String
 	 */
-	public $backupPathWin = "C:\\Backup";
-	
-	/**
+  public $backupPathWin = "C:\\Backup";
+
+  /**
+   * Response Objekt für Logging etc.
+   * @var LibResponseCollector
+   */
+  public $response = null;
+
+  /**
 	 *
 	 * @param LibFlowApachemod $env        	
 	 */
-	public function __construct($env = null) {
-		if ($env) {
-			$this->env = $env;
-		} else {
-			$this->env = WebFrap::$env;
-		}
-	}
-	
-	/**
+  public function __construct ($env = null, $response = null)
+  {
+
+    if ($env) {
+      $this->env = $env;
+    } else {
+      $this->env = WebFrap::$env;
+    }
+    
+    if ($response) {
+      $this->response = $response;
+    } else {
+      $this->response = new LibResponseCollector();
+    }
+  }
+
+  /**
 	 * Trigger für das Backup einer einzelnen Tabelle
 	 *
 	 * @param String $tableName        	
 	 */
-	public function trigger_table($tableName) {
-		echo "Starting Backup of Table " . $tableName . "<br>";
-		
-		$filename = $tableName . "-backup-" . time () . ".csv";
-		
-		$path = implode ('\\', array (
-				$this->backupPathWin,
-				$filename 
-		));
-		
-		$db = $this->env->getDb ();
-		
-		$sql = <<<SQL
+  public function trigger_table ($tableName)
+  {
+
+    echo "Starting Backup of Table " . $tableName . "<br>";
+    
+    $filename = $tableName . "-backup-" . time() . ".csv";
+    
+    $path = implode('\\', array(
+        $this->backupPathWin, 
+        $filename
+    ));
+    
+    $db = $this->env->getDb();
+    
+    $sql = <<<SQL
 		
 		COPY {$tableName}
 		TO '{$path}'
 		CSV HEADER
 		
 SQL;
-		
-		$db->query ($sql);
-	}
-	
-	/**
+    
+    $db->query($sql);
+  }
+
+  /**
 	 * Trigger für das Backup einer kompletten Datenbank
 	 *
 	 * @param String $databaseName        	
 	 */
-	public function trigger_database($databaseName) {
-		echo "Starting Backup of Database: " . $databaseName . "<br>";
-	}
-	
-	/**
+  public function trigger_database ($databaseName)
+  {
+
+    echo "Starting Backup of Database: " . $databaseName . "<br>";
+  }
+
+  /**
 	 * Trigger für das Backup eines gesamten Servers
 	 */
-	public function trigger_all() {
-		echo "Starting Backup of All Data";
-	}
-	
-	public function trigger_destroyWorld() {
-		echo "World destroyed! <br>";
-		return false;
-	}
-	
-	public function trigger_happy($tv) {
-		
-		$result = null;
-		
-<<<<<<< HEAD
-		if($tv == "1") {
-			$result = true;
-		} 
-		if($tv == "0") {
-=======
-		if ($tv == 1) {
-			echo "# Hier kam <b>true</b> raus <br>";
-			$result = true;
-		} 
-		if ($tv == 0) {
-			echo "#Hier kam <b>false</b> raus <br>";
->>>>>>> development
-			$result = false;
-		}
-		
-		return $result;
-		
-	}
-	
-	/**
+  public function trigger_all ()
+  {
+
+    echo "Starting Backup of All Data";
+  }
+
+  public function trigger_destroyWorld ()
+  {
+
+    echo "World destroyed! <br>";
+    return false;
+  }
+
+  /**
+   * Testmethode, 1 = true, 0 = false
+   * @param int $tv
+   * @param LibResponseCollector $response
+   * @return boolean
+   */
+  public function trigger_happy ($tv, $response)
+  {
+
+    $result = false;
+    
+    if ($tv == "1") {
+      $response->addMessage("Eingabe erfolgreich!");
+      $result = true;
+    }
+    if ($tv == "0") {
+      $response->addWarning("Eingabe nicht erfolgreich!");
+      $result = false;
+    }
+    
+    return $result;
+  }
+
+  /**
 	 * (non-PHPdoc)
 	 *
 	 * @see BaseChild::setUser()
 	 */
-	public function setUser($user) {
-		$this->user = $user;
-	}
+  public function setUser ($user)
+  {
+
+    $this->user = $user;
+  }
 }
 
 ?>
