fe8f29a9bf630c16ad8aaf7b13bc950c15374489
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-04-03 10:53:03 +0200

    mockup new search

--- src/wgt/panel/WgtPanelElementSearch_Overlay.php
+++ src/wgt/panel/WgtPanelElementSearch_Overlay.php
@@ -239,23 +239,51 @@ HTML;
               <div class="right" ><a class="wgtac_close_overlay" href="#close-search" >{$iconClose}</a></div>
             </div>
             
-            <div class="wgt-space" style="height:150px;" >
+            <div class="wgt-space" style="min-height:150px;max-height:250px;" >
             
               <div class="left half" >   
               	<h3>Filter</h3>
+              	<div class="left half" >
+              		<label>Message</label>
+                	<ul class="wgt-tree" >
+                		<li><input type="checkbox" /> New Posts</li>
+                		<li><input type="checkbox" /> Important Posts</li>
+                		<li><input type="checkbox" /> Urgent Posts</li>
+                		<li><input type="checkbox" /> Overdue Posts</li>
+                	</ul>
+              	</div>
+              	<div class="inline half" >
+              		<label>Tasks</label>
+                	<ul class="wgt-tree" >
+                		<li><input type="radio" name="task" /> All</li>
+                		<li><input type="radio" name="task" /> Open</li>
+                		<li><input type="radio" name="task" /> Completed</li>
+                		<li><input type="checkbox" /> Action required</li>
+                		<li><input type="checkbox" /> Waiting for action</li>
+                	</ul>
+              	</div>
+              	<div class="wgt-clear" ></div>
               </div>            
               
               <div class="half right" >
               	<h3 style="width:150px;float:left;" >Custom Filter</h3>
               	<div class="right" >
-              		<button class="wgt-button" >Save actual Selection</button>
+              		<button class="wgt-button" ><i class="icon-save" ></i> Save filter</button>
+              		<button class="wgt-button" ><i class="icon-save" ></i> Save filter as </button>
               	</div>
               	<div class="wgt-clear" >&nbsp;</div>
-              	<label></label>
-              	<ul>
-              		<li>Urgent</li>
-              		<li>Send by Joe</li>
-              		<li>Project X 2012</li>
+              	
+              	<ul class="wgt-tree" >
+              		<li>
+              			<button class="wgt-button" ><i class="icon-filter" ></i> Urgent</button> 
+              			| <button class="wgt-button" ><i class="icon-remove" ></i></button>
+              		</li>
+              		<li><button class="wgt-button ui-state-active" ><i class="icon-filter" ></i> Send by Joe</button> 
+              			| <button class="wgt-button" ><i class="icon-remove" ></i></button>
+              		</li>
+              		<li><button class="wgt-button" ><i class="icon-filter" ></i> Project X 2012</button>
+              			| <button class="wgt-button" ><i class="icon-remove" ></i></button>
+              		</li>
               	</ul>
               </div>
 
@@ -275,7 +303,7 @@ HTML;
             		<table>
             			<thead>
             				<tr>
-            					<th style="width:100px;" >A/O</th>
+            					<th style="width:100px;" >And/Or</th>
             					<th style="width:120px;" >Field</th>
             					<th style="width:70px;" >Not</th>
             					<th style="width:120px;" >Condition</th>
@@ -306,6 +334,55 @@ HTML;
             					<td style="text-align:right;" >
             						<button 
             							class="wgt-button" ><i class="icon-plus-sign" ></i></button><button 
+            								class="wgt-button" ><i class="icon-remove-sign" ></i></button>
+            					</td>
+            				</tr>
+            				<tr>
+            					<td style="padding-left:10px;" >
+            						<i class="icon-double-angle-right" ></i> <select>
+            							<option>AND</option>
+            							<option>OR</option>
+            						</select>
+            					</td>
+            					<td style="text-align:right;" >
+            						{$selectboxFields}
+            					</td>
+            					<td style="text-align:center;" >
+            						<input type="checkbox" />
+            					</td>
+            					<td style="text-align:center;" >
+            						{$slctText->element()}
+            					</td>
+            					<td style="text-align:left;" >
+            						<input type="text" />
+            					</td>
+            					<td style="text-align:right;" >
+            						<button 
+            							class="wgt-button" ><i class="icon-plus-sign" ></i></button><button 
+            								class="wgt-button" ><i class="icon-remove-sign" ></i></button>
+            					</td>
+            				</tr>
+										<tr>
+            					<td style="padding-left:30px;" >
+            						<i class="icon-double-angle-right" ></i> <select>
+            							<option>AND</option>
+            							<option>OR</option>
+            						</select>
+            					</td>
+            					<td style="text-align:right;" >
+            						{$selectboxFields}
+            					</td>
+            					<td style="text-align:center;" >
+            						<input type="checkbox" />
+            					</td>
+            					<td style="text-align:center;" >
+            						{$slctText->element()}
+            					</td>
+            					<td style="text-align:left;" >
+            						<input type="text" />
+            					</td>
+            					<td style="text-align:right;" >
+            						<button 
             								class="wgt-button" ><i class="icon-remove-sign" ></i></button>
             					</td>
             				</tr>
@@ -341,10 +418,17 @@ HTML;
     				</div>
     				
           </div>
+          
+          <div class="wgt-clear small wgt-border-bottom" >&nbsp;</div>
+          <div class="wgt-clear tiny" >&nbsp;</div>
+          
+          <div class="full wgt-space" style="text-align:right" >
+          	<button class="wgt button" ><i class="icon-search" ></i> Search</button>
+          </div>
 
-        </div>
+        </div> <!-- end dropdown -->
         
-      </div>
+      </div><!-- end panel right ->
 
 HTML;
 
@@ -361,7 +445,7 @@ HTML;
   protected function renderAdvancedSearchFieldsSelectbox()
   {
     
-    $select = '<select>';
+    $select = '<select class="wcm wcm_widget_selectbox" >';
     
     foreach ( $this->searchFields as $category => $catFields ) {
       
