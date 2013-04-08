cd8a16bced328960bf9756a8c4bf9e2d02552371
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-04-08 16:49:48 +0200

    fixed the inheritance in the AclMgmt_Query

--- module/acl/mgmt/AclMgmt_Query.php
+++ module/acl/mgmt/AclMgmt_Query.php
@@ -26,7 +26,7 @@
  * @author Dominik Bonsch <dominik.bonsch@webfrap.net>
  * @copyright webfrap.net <contact@webfrap.net>
  */
-class AclMgmt_Query extends Webfrap_Acl_Query
+class AclMgmt_Query extends LibSqlQuery
 {
 /*//////////////////////////////////////////////////////////////////////////////
 // fetch methodes
