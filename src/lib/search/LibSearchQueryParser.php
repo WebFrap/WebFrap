ced1f8ac1c8bb8f6195f8ea2e47b952d6253aa28
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-04-12 23:27:20 +0200

    renamed search parser

--- src/lib/search/LibSearchQueryParser.php
+++ src/lib/search/LibSearchQueryParser.php
@@ -19,7 +19,7 @@
  * @package WebFrap
  * @subpackage tech_core
  */
-class LibSearchParser extends LibParser
+class LibSearchQueryParser extends LibParser
 {
 /*//////////////////////////////////////////////////////////////////////////////
 // attribute
