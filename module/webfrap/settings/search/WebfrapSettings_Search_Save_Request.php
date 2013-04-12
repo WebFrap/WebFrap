f109f20ba0031ed39e7344fa669b32778af7a6a0
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-04-12 10:17:53 +0200

    added "persistent search settings" service

---
+++ module/webfrap/settings/search/WebfrapSettings_Search_Save_Request.php
@@ -0,0 +1,45 @@
+<?php
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
+* @date        :
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
+
+/**
+ * @package WebFrap
+ * @subpackage Maintenance
+ * @author Dominik Bonsch <dominik.bonsch@webfrap.net>
+ * @copyright Webfrap Developer Network <contact@webfrap.net>
+ */
+class WebfrapSettings_Search_Save_Request extends Context
+{
+/*//////////////////////////////////////////////////////////////////////////////
+// Aspects
+//////////////////////////////////////////////////////////////////////////////*/
+  
+
+  
+/*//////////////////////////////////////////////////////////////////////////////
+// Methoden
+//////////////////////////////////////////////////////////////////////////////*/
+  
+  /**
+   * @param LibRequestHttp $request
+   */
+  public function interpretRequest($request)
+  {
+
+  }//end public function interpretRequest */
+
+}//end class WebfrapSettings_Search_Save_Request */
+
