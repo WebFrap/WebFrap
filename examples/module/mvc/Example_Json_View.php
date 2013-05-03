984a045caf09621a877faff2f65cacb54a57dec5
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-05-03 13:32:48 +0200

    improved example

--- examples/module/mvc/Example_Json_View.php
+++ examples/module/mvc/Example_Json_View.php
@@ -26,7 +26,7 @@
  * @author Dominik Bonsch <dominik.bonsch@webfrap.net>
  * @copyright webfrap.net <contact@webfrap.net>
  */
-class Example_Json_View extends WgtModal
+class Example_Json_View extends LibTemplateServiceView
 {
 
   /**
