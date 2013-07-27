9eb66b74cde5278172b1e1679b1b0cb7c2145e2a
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-07-27 19:26:09 +0200

    updated the context

--- src/Context.php
+++ src/Context.php
@@ -276,5 +276,207 @@ class Context
     return array_key_exists($key , $this->content);
   }//end public function exists */
 
+  /**
+   * method to set the form data
+   * @param TFlag $param
+   */
+  public function setSearchFormData($view, $subkey = null)
+  {
+
+    $formAction = $this->searchFormAction;
+
+    // the id of the html table where the new entry has to be added
+    if ($this->targetId)
+      $formAction .= '&amp;target_id='.$this->targetId;
+
+    // flag if the new entry should be added with connection action or CRUD actions
+    if ($this->publish)
+      $formAction .= '&amp;publish='.$this->publish;
+
+    // target is a pointer to a js function that has to be called
+    if ($this->target)
+      $formAction .= '&amp;target='.$this->target;
+
+    if ($this->ltype)
+      $formAction .= '&amp;ltype='.$this->ltype;
+
+    // target is a pointer to a js function that has to be called
+    if ($this->input)
+      $formAction .= '&amp;input='.$this->input;
+
+    // suffix is used to prevent namecolisions in form objects for the same
+    // entity, on the same mask but diffrent datasets
+    // suffix is normaly the objid, "search" oder "create"
+    // it has to be transported during selection / filter requests, else the
+    // data method is not able to target the correct input elements
+    if ($this->suffix)
+      $formAction .= '&amp;suffix='.$this->suffix;
+
+    // they keyname is used to prevent naming colissions in forms
+    if ($this->keyName)
+      $formAction .= '&amp;key_name='.$this->keyName;
+
+    // they keyname is used to prevent naming colissions in forms
+    if ($this->fullLoad)
+      $formAction .= '&amp;full_load=true';
+
+    // check if there are things to exclude
+    if ($this->exclude)
+      $formAction .= '&amp;exclude='.$this->exclude;
+
+    // which view type was used, important to close the ui element eg.
+    if ($this->viewType)
+      $formAction .= '&amp;view='.$this->viewType;
+
+    // which view type was used, important to close the ui element eg.
+    if ($this->viewId)
+      $formAction .= '&amp;view_id='.$this->viewId;
+
+    // append objid
+    if ($this->objid)
+      $formAction .= '&amp;objid='.$this->objid;
+
+    if ($this->refIds) {
+      foreach ($this->refIds as $key => $value) {
+        $formAction .= '&amp;refids['.$key.']='.$value;
+      }
+    }
+
+    if ($this->dynFilters) {
+      foreach ($this->dynFilters as $value) {
+        $formAction .= '&amp;dynfilter[]='.$value;
+      }
+    }
+
+    // ACLS
+
+    // startpunkt des pfades für die acls
+    if ($this->aclRoot)
+      $formAction .= '&amp;a_root='.$this->aclRoot;
+
+    // die id des Datensatzes von dem aus der Pfad gestartet wurde
+    if ($this->aclRootId)
+      $formAction .= '&amp;a_root_id='.$this->aclRootId;
+
+    // der key des knotens auf dem wir uns im pfad gerade befinden
+    if ($this->aclKey)
+      $formAction .= '&amp;a_key='.$this->aclKey;
+
+    // der key des knotens auf dem wir uns im pfad gerade befinden
+    if ($this->aclLevel)
+      $formAction .= '&amp;a_level='.$this->aclLevel;
+
+    // der neue knoten
+    if ($this->aclNode)
+      $formAction .= '&amp;a_node='.$this->aclNode;
+
+    // add the action to the form
+    $this->addVar('searchFormAction'.$subkey, $formAction);
+
+    // check if there is a specific class for the crudform, if not use wcm wcm_req_ajax
+    if (!$this->searchFormClass)
+      $this->searchFormClass = 'wcm wcm_req_ajax';
+
+    // add the class to the form
+    $view->addVar('searchFormClass'.$subkey, $this->searchFormClass);
+
+    // formId
+    $view->addVar('searchFormId'.$subkey, $this->searchFormId);
+
+  }//end public function setFormAction */
+
+  /**
+   * method to set the form data
+   * @param TFlag $param
+   */
+  public function buildSearchFormAction($formAction)
+  {
+
+    // the id of the html table where the new entry has to be added
+    if ($this->targetId)
+      $formAction .= '&amp;target_id='.$this->targetId;
+
+    // flag if the new entry should be added with connection action or CRUD actions
+    if ($this->publish)
+      $formAction .= '&amp;publish='.$this->publish;
+
+    // target is a pointer to a js function that has to be called
+    if ($this->target)
+      $formAction .= '&amp;target='.$this->target;
+
+    // target is a pointer to a js function that has to be called
+    if ($this->input)
+      $formAction .= '&amp;input='.$this->input;
+
+    // they keyname is used to prevent naming colissions in forms
+    if ($this->keyName)
+      $formAction .= '&amp;key_name='.$this->keyName;
+
+    // suffix is used to prevent namecolisions in form objects for the same
+    // entity, on the same mask but diffrent datasets
+    // suffix is normaly the objid, "search" oder "create"
+    // it has to be transported during selection / filter requests, else the
+    // data method is not able to target the correct input elements
+    if ($this->suffix)
+      $formAction .= '&amp;suffix='.$this->suffix;
+
+    if ($this->fullLoad)
+      $formAction .= '&amp;full_load=true';
+
+    // check if there are things to exclude
+    if ($this->exclude)
+      $formAction .= '&amp;exclude='.$this->exclude;
+
+    // which view type was used, important to close the ui element eg.
+    if ($this->viewType)
+      $formAction .= '&amp;view='.$this->viewType;
+
+    // which view type was used, important to close the ui element eg.
+    if ($this->viewId)
+      $formAction .= '&amp;view_id='.$this->viewId;
+
+    // append objid
+    if ($this->objid)
+      $formAction .= '&amp;objid='.$this->objid;
+
+    if ($this->refIds) {
+      foreach ($this->refIds as $key => $value) {
+        $formAction .= '&amp;refids['.$key.']='.$value;
+      }
+    }
+
+    if ($this->dynFilters) {
+      foreach ($this->dynFilters as $value) {
+        $formAction .= '&amp;dynfilter[]='.$value;
+      }
+    }
+
+    // ACLS
+
+    // startpunkt des pfades für die acls
+    if ($this->aclRoot)
+      $formAction .= '&amp;a_root='.$this->aclRoot;
+
+    // die id des Datensatzes von dem aus der Pfad gestartet wurde
+    if ($this->aclRootId)
+      $formAction .= '&amp;a_root_id='.$this->aclRootId;
+
+    // der key des knotens auf dem wir uns im pfad gerade befinden
+    if ($this->aclKey)
+      $formAction .= '&amp;a_key='.$this->aclKey;
+
+    // der key des knotens auf dem wir uns im pfad gerade befinden
+    if ($this->aclLevel)
+      $formAction .= '&amp;a_level='.$this->aclLevel;
+
+    // der neue knoten
+    if ($this->aclNode)
+      $formAction .= '&amp;a_node='.$this->aclNode;
+
+    return $formAction;
+
+  }//end public function setFormAction */
+
+
 } // end class Context
 
