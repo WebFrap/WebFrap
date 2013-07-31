c8385fdce00518e52ba6d1a88eab626c7661b202
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-07-31 21:09:46 +0200

    extended period manager added period exception

--- src/lib/period/LibPeriodManager.php
+++ src/lib/period/LibPeriodManager.php
@@ -37,12 +37,12 @@ class LibPeriodManager extends BaseChild
    */
   public function __construct($env = null)
   {
-    
+
     if (!$env)
       $env = Webfrap::$env;
-    
+
     $this->env = $env;
-    
+
   }//end public function __construct */
 
 /*//////////////////////////////////////////////////////////////////////////////
@@ -51,28 +51,47 @@ class LibPeriodManager extends BaseChild
 
   /**
    * Id der aktiven Period für eine bestimmten Type erfragen
-   * 
+   *
    * @param string $key
    */
   public function getActivePeriod($key)
   {
-    
+
     $active = EWbfsysPeriodStatus::ACTIVE;
-    
+
     $sql = <<<SQL
-SELECT 
-  period.rowid 
+SELECT
+  period.rowid
 FROM wbfsys_period period
-  JOIN wbfsys_period_type type 
+  JOIN wbfsys_period_type type
     ON type.rowid = period.id_type
 WHERE
   upper(type.access_key) = upper('{$key}')
   and period.status = {$active};
 SQL;
-    
+
     return $this->getDb()->select($sql)->getField('rowid');
-    
+
   }//end public function getActivePeriod */
-  
+
+
+  public function initNewPeriodType($key)
+  {
+
+    $orm = $this->getOrm();
+
+    $period = $orm->getByKey('WbfsysPeriodType', $key);
+
+    if (!$period)
+      throw new LibPeriod_Exception('Got key '.$key.' to initialize, however this period type does not exist.' );
+
+    // prüfen ob nicht schon initialisiert
+    if ($period->status > 1) {
+      throw new LibPeriod_Exception('The period type '.$key.' is allready initialized');
+    }
+
+
+  }//end public function initNewPeriodType */
+
 }//end class LibPeriodManager
 
