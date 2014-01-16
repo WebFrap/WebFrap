ecf891defba1f7a8b387f36f5bbd5989be061230
Dominik Bonsch <dominik.bonsch@webfrap.net>
2014-01-16 16:29:37 +0100

    User Relation vereinfacht

--- src/e/EUserRelation.php
+++ src/e/EUserRelation.php
@@ -39,35 +39,17 @@ class EUserRelation
   const PARTNER = 2;
   
   /**
-   * Account eines externen users dem wir allerdings trusten
-   * @var int
-   */
-  const TRUSTED = 3;
-  
-  /**
    * Ist uns bekannt (z.B ein guter Kunde)
    * @var int
    */
-  const KNOWN = 4;
+  const CUSTOMER = 3;
   
   /**
    * Jemand der sich angemeldet hat (z.B ein Bewerber)
    * @var int
    */
-  const EXTERN = 5;
+  const EXTERN = 4;
   
-  /**
-   * Jemand der sich annonym angemeldet hat, z.B Forum, oder ein Spider Bot
-   * @var int
-   */
-  const ANNON = 6;
-  
-  /**
-   * Ein Account dessen User wir nicht trauen, den wir stark einschränken möchten
-   * den wir aus welchen Gründen auch immer nicht sperren wollen / können
-   * @var int
-   */
-  const UNTRUSTED = 7;
 
 /*//////////////////////////////////////////////////////////////////////////////
 // Labels
@@ -79,11 +61,8 @@ class EUserRelation
   public static $labels = array(
     self::INTERNAL => 'Internal',
     self::PARTNER => 'Partner',
-    self::TRUSTED => 'Trusted',
-    self::KNOWN => 'Known',
-    self::EXTERN => 'Extern',
-    self::ANNON => 'Annonym',
-    self::UNTRUSTED => 'Un trusted',
+    self::CUSTOMER => 'Customer',
+    self::EXTERN => 'Extern'
   );
 
   /**
@@ -95,7 +74,7 @@ class EUserRelation
     
     return isset(self::$labels[$key])
       ? self::$labels[$key]
-      : self::UNTRUSTED; // sollte nicht passieren, sowas vertrauen wir nicht
+      : ''; // sollte nicht passieren, sowas vertrauen wir nicht
 
   }//end public static function label */
 
